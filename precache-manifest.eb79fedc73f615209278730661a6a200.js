a09e1638c2c60000d21557ea9089f8c198e38383
Lalá Nametala <lalanametala@gmail.com>
2022-04-21 13:47:22 -0300

    Updates

---
+++ precache-manifest.eb79fedc73f615209278730661a6a200.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa929d4c78bf857a00530cfaa1e10d2e",
+    "url": "/star-wars-project/index.html"
+  },
+  {
+    "revision": "5988f544d234c861c92d",
+    "url": "/star-wars-project/static/css/main.224f13b4.chunk.css"
+  },
+  {
+    "revision": "c6456105ea77ffc7d271",
+    "url": "/star-wars-project/static/js/2.0669b23a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-project/static/js/2.0669b23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5988f544d234c861c92d",
+    "url": "/star-wars-project/static/js/main.81ddacb1.chunk.js"
+  },
+  {
+    "revision": "88082d819d89e019d559",
+    "url": "/star-wars-project/static/js/runtime-main.127fcb49.js"
+  },
+  {
+    "revision": "4d642cf99fccaecb2df9af58c439e3a6",
+    "url": "/star-wars-project/static/media/Star_Wars_original_opening_crawl_1977.4d642cf9.mp3"
+  },
+  {
+    "revision": "37da0be547597a78de6b6a606b7be411",
+    "url": "/star-wars-project/static/media/logo.37da0be5.svg"
+  }
+]);
